a73c10141cdd48de740d1582fbdb5a492117c5ff
Jean-Abiad <jeanjesus2501@gmail.com>
2024-07-01 22:46:14 -0400

    home almost completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Phone } from "@/components/Phone";
+
 import { Check, Star } from "lucide-react";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -64,8 +66,8 @@ export default function Home() {
                   />
                 </div>
 
-                <div className="flex flex-col justify-between items-center sm:items-start">
-                  <div className="flex gap-0.5">
+                <div className="flex flex-col text-center justify-between items-center sm:items-start">
+                  <div className="flex  gap-0.5">
                     <Star className="h-4 w-4 text-green-600 fill-green-600"/>
                     <Star className="h-4 w-4 text-green-600 fill-green-600"/>
                     <Star className="h-4 w-4 text-green-600 fill-green-600"/>
@@ -77,7 +79,20 @@ export default function Home() {
               </div>
             </div>
           </div>
-          
+
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img 
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" src="/your-image.png" 
+              />
+              <img 
+                src="/line.png" 
+                className="absolute w-20 -left-6 -bottom-6 select-none" alt="" 
+                />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg " />
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
